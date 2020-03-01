da3d4f94ddf1026d586cfd2b6d171871709682a2
x-kaoki-x <balance877@gmail.com>
2020-03-01 19:03:04 +0900

    TypeScript導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jquery" : true,
-        "jest" : true
-    },
-    "extends": [
-        "airbnb-base",
-        "plugin:vue/essential",
-        "plugin:prettier/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "page": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "prettier/prettier": [
-            "error",
-            {
-                "singleQuote": true
-            }
-        ]
-    }
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jquery" : true,
+    "jest" : true
+  },
+  "extends": [
+    "airbnb-base",
+    "plugin:prettier/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "page": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true
+      }
+    ]
+  }
 };
